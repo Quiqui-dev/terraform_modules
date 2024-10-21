20ae9d4ed8f113ad57c26c0ddec8e78c0168397a
Kieran Mitchell <kieran@Kierans-MacBook-Pro.local>
2024-10-21 14:14:06 +0100

    adding app service plan module and removing merge tags on resource group

--- Azure/resource-group/resource-group.tf
+++ Azure/resource-group/resource-group.tf
@@ -6,9 +6,8 @@ resource "azurerm_resource_group" "service-rg" {
     name = join("-", [var.platform_name, "${upper(substr(var.region, 0, 3))}${substr(var.region, 3 - length(var.region), -1)}", upper(var.environment), format("%.2f", var.environment_version), var.service_name, "rg"])
     location = var.region
 
-    tags = merge(
-        var.persistent_tags
-    )
+    tags = var.persistent_tags
+    
 }
 
 output "rg_name" {
